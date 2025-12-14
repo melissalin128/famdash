fde4416100ce1f51b617fa93ee776b585daf82e6
Melissa Lin <melissalin128@gmail.com>
2025-12-14 22:27:34 +0800

    update vercel.json

--- familycare/vite.config.js
+++ familycare/vite.config.js
@@ -4,9 +4,14 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir: "dist"
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
 });
+
+
